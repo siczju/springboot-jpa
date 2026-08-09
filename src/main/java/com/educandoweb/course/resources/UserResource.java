7e91d2b99100fff0b0824768f3f71029f2ffc7ec
Júlio César <juliocesar.gm6000@gmail.com>
2026-08-09 13:12:29 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,8 +1,11 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +15,20 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}") // indicar que é um GET que recebe um ID na URL
+    public ResponseEntity<User> findById(@PathVariable Long id){ // @PathVariable para o spring mapear da url para o parametro
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.course.services;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get(); // get() é um metodo do Optional que retorna o objeto dentro do Optional
+    }
+
+}
